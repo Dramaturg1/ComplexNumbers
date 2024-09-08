b91052c890a4af792fdb0d57155ab14c5b0572b3
Dramaturg1 <widowmakerNoOver@outlook.com>
2024-09-09 00:17:08 +0300

    added arithmetic operators overloading

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +9,26 @@ namespace ConsoleApp2
 {
     internal class Program
     {
-        class ComplexNumbers
+        class ComplexNumber
         {
-            private double real;
-            private double imag;
-            ComplexNumbers(double real, double imag)
+            private readonly double real;
+            private readonly double imag;
+            ComplexNumber(double real, double imag)
             {
                 this.real = real;
                 this.imag = imag;
             }
 
-            public void Init(double real, double imag)
-            {
-                this.real = real;
-                this.imag = real;
-            }
+            public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b) => new ComplexNumber(a.real + b.real, a.imag + b.imag);
+
+            public static ComplexNumber operator -(ComplexNumber a) => new ComplexNumber(-a.real, -a.imag);
+
+            public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b) => new ComplexNumber(a.real - b.real, a.imag - b.imag);
+
+            public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b) => new ComplexNumber(a.real * b.real - a.imag * b.imag, a.real * b.imag + a.imag * b.real);
+
+            public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b) => new ComplexNumber((a.real * b.real + a.imag * b.imag) / (b.real * b.real + b.imag * b.imag), (a.imag * b.real + a.real * b.imag) / (b.real * b.real + b.imag * b.imag));
+
 
         }
         static void Main(string[] args)
