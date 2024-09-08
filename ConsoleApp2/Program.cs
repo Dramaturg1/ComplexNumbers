7154e33aba1be68561906c75db190e04a72e0ba2
Dramaturg1 <widowmakerNoOver@outlook.com>
2024-09-08 23:55:58 +0300

    added class ComplexNumbers with constructor

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,6 +8,23 @@ namespace ConsoleApp2
 {
     internal class Program
     {
+        class ComplexNumbers
+        {
+            private double real;
+            private double imag;
+            ComplexNumbers(double real, double imag)
+            {
+                this.real = real;
+                this.imag = imag;
+            }
+
+            public void Init(double real, double imag)
+            {
+                this.real = real;
+                this.imag = real;
+            }
+
+        }
         static void Main(string[] args)
         {
         }
